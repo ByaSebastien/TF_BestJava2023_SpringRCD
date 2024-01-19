382d9dd44dfd4098e06546a94c67b34e67a1f40f
Sebastien <byasebastien@hotmail.com>
2024-01-19 16:55:28 +0100

    fix cross origin

--- src/main/java/be/bstorm/tf_bestjava2023_springrcd/pl/controllers/security/AuthenticationController.java
+++ src/main/java/be/bstorm/tf_bestjava2023_springrcd/pl/controllers/security/AuthenticationController.java
@@ -10,10 +10,12 @@ import be.bstorm.tf_bestjava2023_springrcd.pl.utils.JwtUtils;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin("*")
 @RestController
 @RequiredArgsConstructor
 public class AuthenticationController {
